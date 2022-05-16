0e84a30005444aa22bf7c5d5b58489dd01e4b194
Dzaky_Faishalariq003 <dzakyfaishalariq@gmail.com>
2022-05-16 19:56:17 +0700

    latihan

--- latihan_hakerrang_lanjut/tess3.py
+++ latihan_hakerrang_lanjut/tess3.py
@@ -1,5 +1,12 @@
 # macam macam variabel
-
+import os
+os.system("cls")
 nilai = 10
 nilai2 = 10.0
 nilai3 = "String"
+nilai4 = 'u'
+
+print(type(nilai))
+print(type(nilai2))
+print(type(nilai3))
+print(type(nilai4))
