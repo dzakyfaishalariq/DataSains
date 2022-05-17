b4f04fd1b6165e8cdc0774130894add871f7c38d
Dzaky_Faishalariq003 <dzakyfaishalariq@gmail.com>
2022-05-17 14:15:13 +0700

    latihan

---
+++ latihan_hakerrang_lanjut/tes5.py
@@ -0,0 +1,23 @@
+# operator aritmatika
+import os
+import math
+os.system("cls")
+a = 10
+b = 5
+c = a + b  # penjumlahan
+print(c)
+c = a - b  # pengurangan
+print(c)
+c = a * b  # perkalian
+print(c)
+c = a / b  # pembagian
+print(c)
+c = a % b  # sisa hasil bagi
+print(c)
+c = a ** b  # pangkat
+print(a)
+c = a // b  # pembulatan ke bawah
+print(c)
+c = math.sqrt(a)  # akar
+print(c)
+print(math.log(a,10))  # logaritma
